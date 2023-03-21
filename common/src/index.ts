c070193670f160c1f9e6dedab965fac0295b63cd
Elalfy74 <mahmoudelalfy13@gmail.com>
2023-03-21 20:34:05 +0200

    k8s init

--- common/src/index.ts
+++
@@ -1,8 +0,0 @@
-export * from './kafka/consumer.service';
-export * from './kafka/producer.service';
-export * from './kafka/kafka.module';
-export * from './kafka/interfaces';
-
-export * from './prisma/prisma.module';
-export * from './prisma/prisma.service';
-export * from './guards/auth.guard';
