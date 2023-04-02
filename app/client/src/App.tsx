9902de69752c840a3f3dc94bbda8b2bade28bcad
elalfy74 <mahmoudelalfy13@gmail.com>
2023-04-02 03:32:58 +0200

    fixed checkauth

--- app/client/src/App.tsx
+++ app/client/src/App.tsx
@@ -1,6 +1,6 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { MainLayout } from '@/layouts';
 import { ThemeProvider } from './ThemeProvider';
 import { Home } from './pages';
@@ -9,18 +9,21 @@ import { useAuth } from './store';
 const queryClient = new QueryClient();
 
 export default function App() {
+  const [isLoading, setIsLoading] = useState(true);
   const checkAuth = useAuth((state) => state.checkAuth);
 
   useEffect(() => {
-    checkAuth();
+    checkAuth().then(() => setIsLoading(false));
   }, []);
 
   return (
     <QueryClientProvider client={queryClient}>
       <ThemeProvider>
-        <MainLayout>
-          <Home />
-        </MainLayout>
+        {!isLoading && (
+          <MainLayout>
+            <Home />
+          </MainLayout>
+        )}
       </ThemeProvider>
     </QueryClientProvider>
   );
